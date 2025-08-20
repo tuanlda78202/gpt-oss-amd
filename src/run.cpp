19be592cb5ed41962deee4e4c129ed129243e40a
tuanlda78202 <tuanleducanh78202@gmail.com>
2025-08-20 08:34:12 +0000

    chore: update tokenizer path and set omp threads

--- src/run.cpp
+++ src/run.cpp
@@ -1098,7 +1098,7 @@ void error_usage() {
 int main(int argc, char** argv) {
     // default parameters
     char* checkpoint_path = NULL; // e.g. out/model.bin
-    const char* tokenizer_path = "tokenizer.bin";
+    const char* tokenizer_path = "build/tokenizer.bin";
     float temperature = 0.0f; // 0.0 = greedy deterministic. 1.0 = original. don't set higher
     float topp = 0.9f;        // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
     int steps = 1024;         // number of steps to run for
