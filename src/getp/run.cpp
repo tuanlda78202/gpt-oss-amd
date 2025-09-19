0fe3dbfc302ba0809db45d29d8432dc96a82d15a
tuanlda78202 <tuanleducanh78202@gmail.com>
2025-09-19 15:20:04 +0000

    perf: limit seq_len

--- src/getp/run.cpp
+++ src/getp/run.cpp
@@ -23,6 +23,7 @@ static int num_gpus = 1;
 void warm_up(Transformer* transformer, Tokenizer* tokenizer, int batch_size, int use_kv16) {
     OssTransformer* transformer_oss = (OssTransformer*)transformer;
     transformer_oss->config.batch_size = batch_size;
+    transformer_oss->config.seq_len = 1024;
 
     // Discover GPUs
     CHECK_HIP(hipGetDeviceCount(&num_gpus));
